85bb707493eba6e7d8cca39cda22ed21452c24de
UserNameUndetected <th.luisman@student.alfa-college.nl>
2017-11-17 15:00:26 +0100

    Commited Heroku files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Heroku</title>
+</head>
+
+<body>
+<link rel="stylesheet" href="styleSession.css" type="text/css">
+<div class="body-content">
+    <div class="module">
+        <div class="titleHeading">
+            <h1 class="head headerTitle head-primary">Thieme VRDI</h1>
+        </div>
+
+        <?php
+            echo "Stukje text ofzo xD";
+        ?>
+        <a id="hrefRegister" href="formulier.php">
+            <input type="button" value="Verder" name="" class="btn btn-block btn-primary" /></a>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ volgende.php
@@ -0,0 +1,24 @@
+<?php include('volgende.php')?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Heroku</title>
+</head>
+
+<body>
+<link rel="stylesheet" href="styleSession.css" type="text/css">
+<div class="body-content">
+    <div class="module">
+        <div class="titleHeading">
+            <h1 class="head headerTitle head-primary">Heroku</h1>
+
+        </div>
+        <a id="hrefRegister" href="volgende.php">
+            <input type="button" value="Verder" name="" class="btn btn-block btn-primary" /></a>
+    </div>
+</div>
+
+</body>
+</html>
